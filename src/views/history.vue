58f5ad2d27cf2c0613a03a35efb96e4c231cb12a
czqzeot <czqzeot@.com>
2017-12-20 00:26:54 +0800

    tab

--- src/views/history.vue
+++ src/views/history.vue
@@ -1,20 +1,64 @@
-<tempalte>
-    <div>
-
+<template>
+    <div class="container">
+        <div class="tab-item" >
+            <ul>
+                <li v-for="(item,index) in itemTabs" :class="{active: index == num}" @click="tab(index)"  :key="index">{{item}}</li>
+            </ul>
+        </div>
+        <div class="tab-item-box" v-for="(item,index) in itemBoxs" :key="index">
+            <ul v-show=" index == num">
+                <li>{{item}}</li>
+            </ul>
+        </div>
     </div>
-</tempalte>
+</template>
 <script>
     export default {
     data(){
-        return {
+            return {
+                itemTabs:['全部','已完成','待生效'],
+                itemBoxs:['2','3','4'],
+                num:0
+            }
 
-        }
     },
-     methods(){
-
+     methods:{
+        tab(index){
+            this.num = index;
+         }
     }
     }
 </script>
 <style lang="less" scoped="scoped">
-
+    .container{
+        background-color: #fff;
+    }
+    .tab-item{
+        ul{
+            display: flex;
+            align-items: center;
+            padding: .2rem 0;
+            border-bottom: 1px solid #f9f9f9;
+            li{
+                flex:1;
+                text-align: center;
+                /*background-color: red;*/
+                font-size: .32rem;
+                line-height: .4rem;
+                color: rgba(153,153,153,0.60);
+                letter-spacing: 0.21px;
+            }
+            .active{
+                color: #FC8D00;
+            }
+        }
+    }
+    .item-box{
+        ul{
+            li{
+                background-color: #fff;
+                border-bottom:1px solid #f9f9f9;
+            }
+        }
+    }
 </style>
