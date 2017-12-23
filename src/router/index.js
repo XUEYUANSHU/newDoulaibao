ef3cef85ddf5b3e9af26742833db838e9325cdf0
czqzeot <czqzeot@.com>
2017-12-23 10:20:18 +0800

    66

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 
 
 import HomePage from "@/views/index/HomePage"
+import Raider from '@/views/Raider' // 创富攻略
 import Products from "@/views/Products"
 import UserCenter from "@/views/UserCenter"
 import OrderList from "@/views/OrderList"
@@ -11,12 +12,13 @@ import myWealth from "@/views/myWealth"
 import userProfile from '@/views/userProfile'
 import editUserName from '@/views/editUserName'
 import history from '@/views/history'
-import Certification from '@/views/Certification'
+
 import News from '@/views/News'
 import TeamLeader from '@/views/TeamLeader'
 import TeamMember from '@/views/TeamMember'
-// import Raider from '@/views/TeamMember'
+
 import bindMobile from '@/views/bindMobile' //手机绑定
+<<<<<<< HEAD
 import changeMobile from '@/views/changeMobile' //修改手机绑定
 import showMobile from '@/views/showMobile' //修改手机绑定
 import Raider from '@/views/Raider'
@@ -29,7 +31,11 @@ import identifyReview from '@/views/identifyReview' //认证审核中
 import identifySucc from '@/views/identifySucc' //认证审核完成
 import tixianGoIdentify from '@/views/tixianGoIdentify' //提现转实名认证
 
-// import cashWechatSuccess from '@views/cashWechatSuccess.vue'  //提现d到微信提示
+// import cashWechatSuccess from '@views/cashWechatSuccess.vue'  //提现d到微信提示 // 实名认证
+import Certification from '@/views/Certification' // 实名认证
+
+import bindBank from '@/views/bindBank' // 绑定银行卡
+// import cashWechatSuccess from '@views/cashWechatSuccess'  //提现页面
 // import AllProduct from "@/views/index/AllProduct"
 // import ImTeam from "@/views/index/ImTeam"
 // //JoinTeam
@@ -41,103 +47,103 @@ import tixianGoIdentify from '@/views/tixianGoIdentify' //提现转实名认证
 
 Vue.use(Router)
 
-var routeData = [
- {
-    path: '/',
-    name: 'Home',
-    component: HomePage
+var routeData = [{
+	path: '/',
+	name: 'Home',
+	component: HomePage
 },
-// {
-//     path: '/imTeam',
-//     name: 'ImTeam',
-//     component: ImTeam
-// },
-//
-// {
-//     path: '/joinTeam',
-//     name: 'JoinTeam',
-//     component: JoinTeam
-// },
-//
+{
+	path: '/Raider',
+	name: 'Raider',
+	component: Raider
+},
+{
+	path: '/',
+	redirect: '/Home'
+},
+{
+	path: '/Products',
+	name: 'Products',
+	component: Products
+},
+{
+	path: '/UserCenter',
+	name: 'UserCenter',
+	component: UserCenter
+},
+{
+	path: '/OrderList',
+	name: 'OrderList',
+	component: OrderList
+},
+{
+	path: '/myWealth',
+	name: 'myWealth',
+	component: myWealth
+},
 
 {
-    path: '/Raider',
-    name: 'Raider',
-    component: Raider
+	path: '/history',
+	name: 'history',
+	component: history
 },
-{
-    path: '/',
-    redirect: '/Home'
-},
-{
-    path: '/Products',
-    name: 'Products',
-    component: Products
-},
-{
-    path: '/UserCenter',
-    name: 'UserCenter',
-    component: UserCenter
-},
-{
-    path: '/OrderList',
-    name: 'OrderList',
-    component: OrderList
-},
-{
-    path: '/myWealth',
-    name: 'myWealth',
-    component: myWealth
-},
-
-    {
-        path: '/history',
-        name: 'history',
-        component: history
-    },
 
 
 {
-    path: '/News',
-    name: 'News',
-    component: News
+	path: '/News',
+	name: 'News',
+	component: News
 },
 {
-    path: '/TeamLeader',
-    name: 'TeamLeader',
-    component: TeamLeader
+	path: '/TeamLeader',
+	name: 'TeamLeader',
+	component: TeamLeader
 },
 {
-    path: '/TeamMember',
-    name: 'TeamMember',
-    component: TeamMember
+	path: '/TeamMember',
+	name: 'TeamMember',
+	component: TeamMember
 },
 {
-    path: '/userProfile',
-    name: 'userProfile',
-    component: userProfile
+	path: '/userProfile',
+	name: 'userProfile',
+	component: userProfile
 },
 {
-    path: '/editUserName',
-    name: 'editUserName',
-    component: editUserName
+	path: '/editUserName',
+	name: 'editUserName',
+	component: editUserName
 },
 
-    {
-        path: '/OrderList',
-        name: 'OrderList',
-        component: OrderList
-    },
-    {
-        path: '/bindMobile',
-        name: 'bindMobile',
-        component: bindMobile
-    },
-
 {
-    path: '/Certification',
-    name: 'Certification',
-    component: Certification
+	path: '/OrderList',
+	name: 'OrderList',
+	component: OrderList
+},
+{
+	path: '/bindMobile',
+	name: 'bindMobile',
+	component: bindMobile
+},
+{
+	path: '/Certification',
+	name: 'Certification',
+	component: Certification
+},
+{
+	path: '/Deposit',
+	name: 'Deposit',
+	component: Deposit
+},
+{
+	path: '/cashWechatSuccess',
+	name: 'cashWechatSuccess',
+	component: cashWechatSuccess
+},
+{
+	path: '/bindBank',
+	name: 'bindBank',
+	component: bindBank
 },
     {
         path: '/Certification',
@@ -198,7 +204,7 @@ var routeData = [
 
 
 var router = new Router({
-    routes: routeData
+	routes: routeData
 })
 
 
